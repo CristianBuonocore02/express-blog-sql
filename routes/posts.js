b28adcade8073922ed192b21544c806e4f32c1e4
Cristian Buonocore <cristian.buonocore680@gmail.com>
2025-07-14 01:59:51 +0200

    feat: implement database query for fetching a single post by ID

--- routes/posts.js
+++ routes/posts.js
@@ -25,21 +25,44 @@ const index = (req, res) => {
 }
 
 // SHOW
-router.get('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find(p => p.id === id);
+// router.get('/:id', (req, res) => {
+//     const id = parseInt(req.params.id);
+//     const post = posts.find(p => p.id === id);
 
-    if (!post) {
-        res.status(404);
-        return res.json({
-            error: 'Not Found',
-            message: 'Post non trovato'
-        });
-    }
+//     if (!post) {
+//         res.status(404);
+//         return res.json({
+//             error: 'Not Found',
+//             message: 'Post non trovato'
+//         });
+//     }
 
-    res.json(post);
-});
+//     res.json(post);
+// });
 
+const show = (req, res) => {
+    console.log(req.params);
+    console.log(typeof req.params.id);
+
+    const id = parseInt(req.params.id)
+
+    const sql = 'SELECT * FROM blog.posts WHERE id = ? ;'
+    console.log(sql);
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: true, message: err.message })
+        console.log(results);
+
+        if (!results.length > 0) {
+            return res.status(404).json({
+                error: true,
+                message: 'Not Found'
+            })
+        }
+        return res.json(results[0])
+
+    })
+}
 
 //POST
 router.post("/", (req, res) => {
